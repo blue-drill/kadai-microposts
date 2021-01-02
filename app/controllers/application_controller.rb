4800c13a7892cbaa4acd3c11605ae1579931883c
shotaro.aoki <aotaro1994@gmail.com>
2021-01-02 01:09:16 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  
+  include SessionsHelper
+  
+  private
+  
+  def require_user_logged_in
+    # unlessはifの反対。falseの時に処理を実行する
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
